0ee00d2c23c26f1a822bbcbc60d1c8a11f9df4b7
scammi <scammi@gmail.com>
2022-05-30 17:43:16 -0300

    Clean

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,17 +1,15 @@
-
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// Charged Particles imports
 const ChargedSettingsAbi = require("@charged-particles/protocol-subgraph/abis/ChargedSettings.json");
 const ChargedParticlesAbi = require("@charged-particles/protocol-subgraph/abis/ChargedParticles.json");
-
 const chargedParticlesMainnetAddress = require("@charged-particles/protocol-subgraph/networks/mainnet.json");
 
-describe("Greeter", function () {
+describe("Charged Particles whitelist ", function () {
   const chargedSettingsMainnetAddress = chargedParticlesMainnetAddress.chargedSettings.address; 
   const chargedParticlesContractMainnetAddress = chargedParticlesMainnetAddress.chargedParticles.address; 
 
-  // const provider = ethers.getDefaultProvider(1);
   const provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL_MAINNET, 1);
 
   const ChargedParticlesContract = new ethers.Contract(chargedParticlesContractMainnetAddress, ChargedParticlesAbi);
@@ -23,30 +21,31 @@ describe("Greeter", function () {
   });
 
   it ("Become admin !", async() => {
-    const CustomNFT = await ethers.getContractFactory("CustomeToken"); 
+    // Deploy custom NFT
+    const CustomNFT = await ethers.getContractFactory("CustomToken"); 
     const customNFT = await CustomNFT.deploy();
-
     const deployed = await customNFT.deployed();
+
     const deploymentAddress = deployed.address;
 
+    // this should revert since you are not the admin
     // const [ signer ] = await ethers.getSigners();
-
-    // this should revert
     // const whiteList = await ChargedSettingContract.connect(signer).enableNftContracts([deploymentAddress])
     
+    // Get Charged Particle owner address
     const adminAddress = await ChargedParticlesContract.connect(provider).owner();
 
+    // impersonate adming account 
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [adminAddress],
     });
 
+    // White list custom NFT
     const owner = await ethers.getSigner(adminAddress);
-
     const whiteList = await ChargedSettingContract.connect(owner).enableNftContracts([deploymentAddress])
+    await whiteList.wait();
+
     console.log(whiteList)
-
-
-
   });
 });
